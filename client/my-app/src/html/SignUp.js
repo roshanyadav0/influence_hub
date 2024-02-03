54eebbc88b2fea8596bc267caf61e21f5fa24121
Roshan Yadav <ry9826653@gmail.com>
2024-02-03 12:59:31 +0530

    sign up backend added

--- client/my-app/src/html/SignUp.js
+++ client/my-app/src/html/SignUp.js
@@ -27,7 +27,7 @@ function SignUp() {
         e.preventDefault();
     
         try {
-            const response = await fetch('http://localhost:5000/app/user/signup', {
+            const response = await fetch('https://influence-hub.onrender.com/app/user/signup', {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',

ef6118f991794b2757fb4803502bff374a1aca65
Roshan Yadav <ry9826653@gmail.com>
2024-02-12 10:57:29 +0530

    responsive bug solverd

--- client/my-app/src/html/Navbar.js
+++ client/my-app/src/html/Navbar.js
@@ -32,41 +32,42 @@ function Navbar() {
                         </div>
                     </div>
                 <div>
-
-                    {console.log({username})}
-                {username ? (
-                        <div id='nav-div-2'>
-                            <p>Hello, {username}!</p>
-                            <button id='button-mui' onClick={()=>navigate('/')}>LOGOUT</button>
-                            <button id="btn1" variant="contained" size="large" color='primary' onClick={()=>navigate('/post')}>Post Request</button>
-                        </div>
-                    ) : (
-                        <div id='nav-div-2'>
-                            <Button id='button-mui' onClick={()=>navigate('/login')}>LOGIN</Button>
-                            <Button id='button-mui' onClick={()=>navigate('/signup')}>SIGN UP</Button>
-                            <Button id="btn1" variant="contained" size="large" color='primary' onClick={()=>navigate('/post')}>Post Request</Button>
-                        </div>
-                    )}
-                </div>
-                <div className="drop-down-menu">
-                    <img
-                    src='./3dot.jpg' // Replace with your image URL
-                    alt="Dropdown Trigger"
-                    onClick={handleToggleDropdown}
-                    />
-                    {isDropdownVisible && (
-                    <div className="dropdown">
-                        {/* Dropdown content goes here */}
-                            <button id='drop-down-button' onClick={()=>navigate('/')}>HOME</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/guide')}>HOW IT WORKS</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/features')}>FEATURES</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/user')}>FIND INFLUENCERS</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/profile')}>PROFILE</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/chat')}>CHAT</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/post')}>POST</button>
-                            <button id='drop-down-button' onClick={()=>navigate('/about')}>ABOUT US</button>
+                <div className='main-div-1-buttons'>
+                        {console.log({username})}
+                    {username ? (
+                            <div id='nav-div-2'>
+                                <p>Hello, {username}!</p>
+                                <button id='button-mui' onClick={()=>navigate('/')}>LOGOUT</button>
+                                <button id="btn1" variant="contained" size="large" color='primary' onClick={()=>navigate('/post')}>Post Request</button>
+                            </div>
+                        ) : (
+                            <div id='nav-div-2'>
+                                <Button id='button-mui' onClick={()=>navigate('/login')}>LOGIN</Button>
+                                <Button id='button-mui' onClick={()=>navigate('/signup')}>SIGN UP</Button>
+                                <Button id="btn1" variant="contained" size="large" color='primary' onClick={()=>navigate('/post')}>Post Request</Button>
+                            </div>
+                        )}
                     </div>
-                    )}
+                    <div className="drop-down-menu">
+                        <img
+                        src='./3dot.jpg' // Replace with your image URL
+                        alt="Dropdown Trigger"
+                        onClick={handleToggleDropdown}
+                        />
+                        {isDropdownVisible && (
+                        <div className="dropdown">
+                            {/* Dropdown content goes here */}
+                                <button id='drop-down-button' onClick={()=>navigate('/')}>HOME</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/guide')}>HOW IT WORKS</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/features')}>FEATURES</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/user')}>FIND INFLUENCERS</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/profile')}>PROFILE</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/chat')}>CHAT</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/post')}>POST</button>
+                                <button id='drop-down-button' onClick={()=>navigate('/about')}>ABOUT US</button>
+                        </div>
+                        )}
+                    </div>
                 </div>
             </div>
     </div>
